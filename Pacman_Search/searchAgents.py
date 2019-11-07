dd528f2f35cf07f5575fd45f0ad3581d79fb9ea0
Socaci Radu <radusocaci@gmail.com>
2019-11-07 13:07:29 +0200

    suboptimal search (#11)

--- Pacman_Search/searchAgents.py
+++ Pacman_Search/searchAgents.py
@@ -527,7 +527,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.bfs(problem)
 
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -564,7 +564,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x, y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 
 def mazeDistance(point1, point2, gameState):
