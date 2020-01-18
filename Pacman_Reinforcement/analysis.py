971409f3119b27f213f43b57ec3009af269fca43
NeaguLorena <lorerocks@yahoo.com>
2020-01-18 22:57:21 +0200

    added Q1, Q2, Q3 max score

--- Pacman_Reinforcement/analysis.py
+++ Pacman_Reinforcement/analysis.py
@@ -21,41 +21,41 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.2
+    answerNoise = 0.01
     return answerDiscount, answerNoise
 
-def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3a(): #prefer the close exit (+1), risking the cliff (-10)
+    answerDiscount = 0.2
+    answerNoise = 0
+    answerLivingReward = -2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3b(): #prefer the close exit (+1), but avoiding the cliff (-10)
+    answerDiscount = 0.5
+    answerNoise = 0.4
+    answerLivingReward = -2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3c(): #prefer the distant exit (+10), risking the cliff (-10)
+    answerDiscount = 0.9
+    answerNoise = 0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3d(): #prefer the distant exit (+10), avoiding the cliff (-10)
+    answerDiscount = 0.8
+    answerNoise = 0.4
+    answerLivingReward = -0.4
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3e(): #avoid both exits and the cliff (so an episode should never terminate)
+    answerDiscount = 0.9
+    answerNoise = 0
+    answerLivingReward = 1.0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
--- Pacman_Reinforcement/valueIterationAgents.py
+++ Pacman_Reinforcement/valueIterationAgents.py
@@ -12,9 +12,10 @@
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
-import mdp, util
+import mdp, util, copy
 
 from learningAgents import ValueEstimationAgent
+
 
 class ValueIterationAgent(ValueEstimationAgent):
     """
@@ -25,7 +26,8 @@ class ValueIterationAgent(ValueEstimationAgent):
         for a given number of iterations using the supplied
         discount factor.
     """
-    def __init__(self, mdp, discount = 0.9, iterations = 100):
+
+    def __init__(self, mdp, discount=0.9, iterations=100):
         """
           Your value iteration agent should take an mdp on
           construction, run the indicated number of iterations
@@ -41,11 +43,26 @@ class ValueIterationAgent(ValueEstimationAgent):
         self.mdp = mdp
         self.discount = discount
         self.iterations = iterations
-        self.values = util.Counter() # A Counter is a dict with default 0
+        self.values = util.Counter()  # A Counter is a dict with default 0
 
         # Write value iteration code here
-        "*** YOUR CODE HERE ***"
 
+        #Computes V*(s) (with Bellman Equation) - expected utility starting in s and acting optimally
+        valuesNext = util.Counter()
+        states = mdp.getStates()
+
+        for i in range(0, self.iterations): # for the number of iterations given
+            for state in states:
+                values = []
+                if self.mdp.isTerminal(state):
+                    values.append(0)
+                #compute possible actions from Q-states
+                possibleActions = mdp.getPossibleActions(state)
+                for action in possibleActions:
+                    values.append(self.computeQValueFromValues(state, action)) #reward + discout * value of state
+                valuesNext[state] = max(values) #maximum value of qstates
+
+            self.values = valuesNext.copy() #put in values a copy of the counter, update for each iteration
 
     def getValue(self, state):
         """
@@ -53,14 +70,20 @@ class ValueIterationAgent(ValueEstimationAgent):
         """
         return self.values[state]
 
-
     def computeQValueFromValues(self, state, action):
         """
           Compute the Q-value of action in state from the
           value function stored in self.values.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        #Computes Q*(state,action) - expected utility that is taking action a from a state s and acts optimally
+        transitionStatesAndProbs = self.mdp.getTransitionStatesAndProbs(state, action)
+        value = 0
+
+        for nextSt, probability in transitionStatesAndProbs: #uses Bellman Equation to compute Q values
+            value += probability * (self.mdp.getReward(state, action, nextSt) + self.discount * self.getValue(nextSt))
+        return value
 
     def computeActionFromValues(self, state):
         """
@@ -72,7 +95,19 @@ class ValueIterationAgent(ValueEstimationAgent):
           terminal state, you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        possibleActions = self.mdp.getPossibleActions(state)
+        if len(possibleActions) == 0:
+            return None
+
+        policy = 0
+        maxVal = 0
+        for action in possibleActions:
+            value = self.computeQValueFromValues(state, action)
+            if value > maxVal or maxVal == 0: #find the maximum of values
+                maxVal = value
+                policy = action
+
+        return policy #return the optimal action with the best Q-value
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
@@ -83,3 +118,4 @@ class ValueIterationAgent(ValueEstimationAgent):
 
     def getQValue(self, state, action):
         return self.computeQValueFromValues(state, action)
+
